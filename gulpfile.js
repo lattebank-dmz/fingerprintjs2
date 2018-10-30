4773eecb023d3ac28b9bad9f8d2da006039a4f67
hbrls <shuaizhexu@gmail.com>
2018-10-30 14:39:59 +0800

    [nice] gulp rev

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 const gulp = require('gulp')
-const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
-const license = require('gulp-header-license')
-const fs = require('fs')
-const year = (new Date()).getFullYear()
+const rev = require('gulp-rev')
+
 
 gulp.task('minify', function () {
   return gulp
       .src('fingerprint2.js')
-      .pipe(rename({suffix: '.min'}))
+      .pipe(rev())
       .pipe(uglify({
         compress: {
           global_defs: {}
@@ -16,11 +14,9 @@ gulp.task('minify', function () {
         output: {
           ascii_only: true
         }
-      }).on('error', function (e) {
+      })
+      .on('error', function (e) {
         console.log(e)
       }))
-      .pipe(license(fs.readFileSync('license.txt', 'utf8'), {YEAR: year}, 0.9))
       .pipe(gulp.dest('dist/'))
 })
-
-gulp.task('default', gulp.series('minify', function (done) { done() }))
